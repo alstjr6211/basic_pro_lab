1a3f06c220076c5585ea3382f4187e837e39ef9d
MinseokYu <alstjr6211@gmail.com>
2024-10-18 23:27:02 +0900

    first commit on calculator

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Caculator extends StatefulWidget {
+  const Caculator({super.key});
+
+  @override
+  State<Caculator> createState() => _CaculatorState();
+}
+
+class _CaculatorState extends State<Caculator> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
