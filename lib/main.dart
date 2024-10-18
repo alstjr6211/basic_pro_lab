bed84548f08d33b4581ce8feb8095b94d6b5673e
MinseokYu <alstjr6211@gmail.com>
2024-10-18 23:29:58 +0900

    f c c

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-
-class Caculator extends StatefulWidget {
-  const Caculator({super.key});
-
-  @override
-  State<Caculator> createState() => _CaculatorState();
+//git checkout -b branch_name 으로 다른 branch로 이동 할 수 있음햣
+void main() {
+  runApp(const MyApp());
 }
 
-class _CaculatorState extends State<Caculator> {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Calculator',
+
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
   }
 }
+
